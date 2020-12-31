69811ae6622a486de88b816e633611ffb7edf37a
Navneet Singh <53988436+Navneet-Singh-123@users.noreply.github.com>
2020-12-31 13:40:06 +0530

    Created APIs for getting the upcoming wemeet, all upcoming and all past

--- routes/wemeet.js
+++ routes/wemeet.js
@@ -17,15 +17,23 @@ const {
   GetAllSession,
   AddSessionSpeakers,
   RemoveSessionSpeakers,
+  Upcoming, 
+  GetAllUpcomingWemeets, 
+  GetAllPastWeMeets
 } = require("../controllers/wemeet");
 
 router.post("/:id/create", CreateWeMeet);
 router.get("/:id/:wemeetid/summary", GetWeMeet);
 router.put("/:id/:wemeetid/update", UpdateWeMeet);
 router.get("/:id/allwemeets", GetAllWeMeets);
+router.get("/:id/upcoming", Upcoming);
+router.get('/:id/allupcoming', GetAllUpcomingWemeets)
+router.get('/:id/allpast', GetAllPastWeMeets);
+
 router.get("/:id/:wemeetid/allspeakers", GetAllSpeakers);
 router.post("/:id/:wemeetid/addspeakers", AddSpeakers);
 router.delete("/:id/:wemeetid/removespeakers", RemoveSpeakers);
+
 router.get("/:id/:wemeetid/allregistrants", GetAllRegistrants);
 router.post("/:id/:wemeetid/addregistrants", AddRegistrants);
 router.post("/:id/:wemeetid/create", CreateSession);
@@ -37,5 +45,6 @@ router.delete(
   "/:id/:wemeetid/:sessionid/removespeakers",
   RemoveSessionSpeakers
 );
+
 
 module.exports = router;
